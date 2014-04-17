5691aaee1ff5d09e0d164ce5f8d97f7e15db09c4
fromeijn <romeijn.f@gmail.com>
2014-04-17 12:51:13 +0200

    mod voor ev3 pad toevoegen
    voor het toevoegen van het pad aan elke regel

---
+++ brixc_mod/add_path.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+FILE * InputFile;
+
+#define BufSize 256
+char buf[BufSize][BufSize];
+
+char addres[BufSize] = "C:\\BricxCC\\API\\";
+
+int main (int argc, char* argv[]) {
+
+	InputFile = fopen (argv[1], "r+" );
+	if (InputFile==NULL) {printf("Error opening InputFile\n"); exit (1);}
+
+	int line = 0;
+	while(fscanf(InputFile, "%s", buf[line]) != EOF){
+		line++;
+	}
+
+	rewind(InputFile);
+
+	int eind = line;
+	line = 0;
+	while(line<eind){
+		fprintf(InputFile, "%s%s\n", addres, buf[line]);
+		line++;
+	}
+	
+	fclose (InputFile);
+	return 0;
+}
+
