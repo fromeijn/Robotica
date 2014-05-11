20336b15013f654fcc3f797795cd6310d03fabc6
fromeijn <romeijn.f@gmail.com>
2014-05-11 22:16:33 +0200

    werkt nog niet naar behoren

--- Xmega/TWI_NXT.c
+++ Xmega/TWI_NXT.c
@@ -1,26 +1,33 @@
 /*
- * TWI_NXT.c
+ * CFile1.c
  *
- * Created: 7-5-2014 12:10:23
+ * Created: 8-5-2014 13:15:42
  *  Author: Floris
- */ 
+ */
+#define F_CPU     2000000UL
 
-#define F_CPU 2000000UL
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
 
-#include "avr_compiler.h"
+#define ENABLE_UART_D0  1
+#define D0_BAUD			115200
+#define D0_CLK2X		0
+
+#include "uart.h"
 #include "twi_slave_driver.h"
 
 #define SLAVE_ADDRESS 0x01
 TWI_Slave_t twiSlave;
 
-char indata[];
+char str[256];
+char indata[16];
+uint8_t index;
 
 void TWIC_SlaveProcessData(void)
 {
-	uint8_t index = twiSlave.bytesReceived;
+	index = twiSlave.bytesReceived;
 	indata[index] = twiSlave.receivedData[index];
 }
 
@@ -28,29 +35,31 @@ int main(void)
 {
 	PORTE.DIRSET = PIN0_bm;
 	PORTE.OUTSET = PIN0_bm;
+	
 	TWI_SlaveInitializeDriver(&twiSlave, &TWIC, TWIC_SlaveProcessData);
 	TWI_SlaveInitializeModule(&twiSlave, SLAVE_ADDRESS, TWI_SLAVE_INTLVL_LO_gc);
-
-	PMIC.CTRL |= PMIC_LOLVLEN_bm;
+	
+	init_uart(&uartD0, &USARTD0, F_CPU, D0_BAUD, D0_CLK2X);
+	PMIC.CTRL = PMIC_LOLVLEN_bm;
 	sei();
 	
-	twiSlave.sendData[0] = 'N';
-	twiSlave.sendData[1] = 'u';
-	twiSlave.sendData[2] = 'm';
-	twiSlave.sendData[3] = 'm';
-	twiSlave.sendData[4] = 'e';
-	twiSlave.sendData[5] = 'r';
-	twiSlave.sendData[6] = ' ';
-	
-	while (1) {
+	twiSlave.sendData[0] = 'H';
+	twiSlave.sendData[1] = 'o';
+	twiSlave.sendData[2] = 'i';
+	while(1)
+	{
 		for (int x = 0; x<10; x++)
 		{
-			twiSlave.sendData[7] = '0'+x;
+			twiSlave.sendData[4] = '0'+x;
+			uart_putc(&uartD0, '0'+x);
+			uart_putc(&uartD0, '\n');
+			uart_putc(&uartD0, '\r');
+			sprintf(str, "byte = 0x%02x\n\r", indata[index]);
+			uart_puts(&uartD0, str);
 			_delay_ms(1000);
 		}
 	}
 }
-
 
 ISR(TWIC_TWIS_vect)
 {
