c7e81fc0f351dbfe04b5517bc9cb04dadf363383
jos preeker <jos@preeker.com>
2014-05-20 12:07:31 +0200

    ontwijkroutine
    defines, ontwijkroutine wat aangepast

--- NXT/ontwijkfunctie voor navigatie programma.c
+++ NXT/ontwijkfunctie voor navigatie programma.c
@@ -1,80 +1,103 @@
+#define ONTWIJKZIJDE 900
+#define ONTWIJKLENGTE 1600
+#define ONTWIJKHOEK 520
 
+unsigned int motorLeft = OUT_B;
+unsigned int motorRight = OUT_A;
 
-void ontwijkobject (void)
+void ontwijkroutine(void)
 {
 
- //naar rechts 
- //    |  
+ //naar rechts
+ //    |
  //    .-.
  //    X |
  //    R-.
  //    |
- OnFwd(motorLeft, 60);
- OnFwd(motorRight, -60);
- Wait(300);
- 
+ OnFwd(motorLeft, 100);
+ OnFwd(motorRight, 0);
+ Wait(ONTWIJKHOEK);
+
  //rechtdoor
- //    |  
+ //    |
  //    .-.
  //    X |
  //    .R.
  //    |
  OnFwd(motorLeft, 100);
  OnFwd(motorRight, 100);
- Wait(500);
- 
+ Wait(ONTWIJKZIJDE);
+
  //naar links
- //    |  
+ //    |
  //    .-.
  //    X |
  //    .-R
  //    |
- OnFwd(motorLeft, -60);
- OnFwd(motorRight, 60);
- Wait(300);
- 
+ OnFwd(motorLeft, 0);
+ OnFwd(motorRight, 100);
+ Wait(ONTWIJKHOEK);
+
  //rechtdoor
- //    |  
+ //    |
  //    .-.
  //    X R
  //    .-.
  //    |
  OnFwd(motorLeft, 100);
  OnFwd(motorRight, 100);
- Wait(1000);
- 
+ Wait(ONTWIJKLENGTE);
+
  //naar links
- //    |  
+ //    |
  //    .-R
  //    X |
  //    .-.
  //    |
- OnFwd(motorLeft, -60);
- OnFwd(motorRight, 60);
- Wait(300);
- 
+ OnFwd(motorLeft, 0);
+ OnFwd(motorRight, 100);
+ Wait(ONTWIJKHOEK);
+
  //rechtdoor
- //    |  
+ //    |
  //    .R.
  //    X |
  //    .-.
  //    |
- 
+
  OnFwd(motorLeft, 100);
  OnFwd(motorRight, 100);
- Wait(500);
- 
+ Wait(ONTWIJKZIJDE);
+
  //naar rechts
- //    |  
+ //    |
  //    R-.
  //    X |
  //    .-.
  //    |
- 
- OnFwd(motorLeft, 60);
- OnFwd(motorRight, -60);
- Wait(300);
- 
 
+ OnFwd(motorLeft, 100);
+ OnFwd(motorRight, 0);
+ Wait(ONTWIJKHOEK);
+ 
+ //rechtdoor
+ //    R
+ //    .-.
+ //    X |
+ //    .-.
+ //    |
+
+ OnFwd(motorLeft, 100);
+ OnFwd(motorRight, 100);
+ Wait(1000);
+
+
+
+}
+
+task main()
+{
+
+	ontwijkroutine();
 
 }
